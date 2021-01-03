bb3e3d7f62babd8d7caab086814a5b6dbb3bb3ad
Ryan Craig Martin <ryan@ryanmartin.io>
2021-01-03 16:22:23 -0500

    initial deploy

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,83 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Ryan Craig Martin</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+          integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <body>
+        <div class="wrap">
+          <div class="profile">
+            <h1 class="profile_name">Ryan Craig Martin</h1>
+            <h2 class="at">@ryancraigmartin</h2>
+            {/* <div class="card"></div> */}
+          </div>
+          <div class="links shake">
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/ryancraigmartin" class="link">
+                  <i class="fab fa-instagram"></i> Instagram
+                </a>
+              </li>
+              <li>
+                <a href="https://www.twitter.com/ryancraigmartin" class="link">
+                  <i class="fab fa-twitter"></i> Twitter
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/ryancraigmartin/" class="link">
+                  Linked<i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://open.spotify.com/user/1230181600?si=224fb6f0b902456f" class="link">
+                  <i class="fab fa-spotify"></i> Follow on Spotify
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://open.spotify.com/playlist/1jzF8m0S75XQB5mPtq6Q7f?si=585cb45b69764772"
+                  class="link"
+                >
+                  <i class="fas fa-headphones-alt"></i> Discover New Music
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/ryancraigmartin" class="link">
+                  <i class="fab fa-github"></i> Github
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/channel/UCvV0JZfZDxo_o-tz9VZz2hw" class="link">
+                  <i class="fab fa-youtube"></i> Subscribe on YouTube
+                </a>
+              </li>
+              <li>
+                <a href="mailto:contact@ryanmartin.io" subject="👋🏼" class="link">
+                  <i class="far fa-envelope"></i> Email Me
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* <div class="card"></div> */}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </body>
+    </>
   )
 }
