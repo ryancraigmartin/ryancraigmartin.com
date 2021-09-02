c7c6c39759baaaff6a572890040ea92867f3a29c
Ryan Craig Martin <ryan@ryanmartin.io>
2021-09-02 15:27:13 -0400

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.scss'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-function MyApp({ Component, pageProps }) {
+import * as googleAnalytics from '../lib/analytics'
+
+function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      googleAnalytics.pageView(url)
+    }
+    // When the component is mounted, subscribe to router changes and log page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
