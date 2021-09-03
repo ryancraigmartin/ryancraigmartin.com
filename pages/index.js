06663b5a7b2f59c76b7dc1d6fc17c7305576ebee
Ryan Craig Martin <ryan@ryanmartin.io>
2021-09-02 21:55:52 -0400

    update profile photo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import profilePhoto from '../public/images/profile.jpg'
+import profilePhoto from '../public/images/profile.png'
 
 export default function Home() {
   return (
