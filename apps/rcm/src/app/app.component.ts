cbc8cb360825a6e3aa999ee1384721e8143bf422
Ryan Craig Martin <ryan@ryanmartin.io>
2022-06-09 08:23:38 -0400

    fix: links component

--- apps/rcm/src/app/app.component.ts
+++ apps/rcm/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core'
-import { Router } from '@angular/router'
-// import { Message } from '@ryancraigmartin/api-interfaces'
 
 @Component({
   selector: 'ryancraigmartin-root',
@@ -13,6 +10,5 @@ import { Router } from '@angular/router'
   `,
 })
 export class AppComponent {
-  // hello$ = this.http.get<Message>('/api/hello')
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor() {}
 }
